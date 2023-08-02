1aaad34862af2928d0e7c9f891668aae24dfd3ca
go2023 <tetyanago2018@gmail.com>
2023-08-02 11:21:42 -0700

    NavBar structure adjustment

--- src/Nav.js
+++ src/Nav.js
@@ -14,12 +14,12 @@ const Nav = ( {search, setSearch }) => {
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                 />
+            </form>
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/post">Post</Link></li>
                     <li><Link to="/about">About</Link></li>
                 </ul>
-            </form>
         </nav>
     );
 };
